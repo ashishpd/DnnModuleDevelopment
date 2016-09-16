a9c99d214d1cd83d2a7e6136778c095e91a79bb1
Ash Prasad <ashishpd@hotmail.com>
2016-09-16 12:42:22 -0700

    initial commit

---
+++ 01-HelloWorld/Modules/Dnn.Modules.HelloWorld/View.ascx
@@ -0,0 +1,4 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="View.ascx.cs" Inherits="Dnn.Modules.HelloWorld.View" %>
+<asp:Panel runat="server">
+    Hello World
+</asp:Panel>
---
+++ 01-HelloWorld/Modules/Dnn.Modules.HelloWorld/View.ascx.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DotNetNuke.Entities.Modules;
+
+namespace Dnn.Modules.HelloWorld
+{
+    public partial class View : PortalModuleBase
+    {
+    }
+}
