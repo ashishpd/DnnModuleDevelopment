bb4485c7bb2ade6da4b31777fa827bd858586789
Ash Prasad <ashishpd@hotmail.com>
2016-09-23 12:02:09 -0700

    Added Settings Control

---
+++ 02-HelloWorldWithSetting/Modules/Dnn.Modules.HelloWorld/View.ascx
@@ -0,0 +1,5 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="View.ascx.cs" Inherits="Dnn.Modules.HelloWorld.View" %>
+<asp:Panel runat="server">
+    Hello World
+    <asp:Label runat="server" ID="lblDays"></asp:Label>
+</asp:Panel>
---
+++ 02-HelloWorldWithSetting/Modules/Dnn.Modules.HelloWorld/View.ascx.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using DotNetNuke.Entities.Modules;
+
+namespace Dnn.Modules.HelloWorld
+{
+    public partial class View : PortalModuleBase
+    {
+        protected override void OnLoad(EventArgs e)
+        {
+            base.OnLoad(e);
+
+            lblDays.Text = Settings.ContainsKey("Days") ? Settings["Days"].ToString() : "5";            
+        }
+    }
+}
